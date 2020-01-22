78f6f5d8b6e473e71ac85ecc7a0db82e725b1a98
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-22 21:18:51 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dancer
+  extend MetaDancing
+ end
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+end
+  
